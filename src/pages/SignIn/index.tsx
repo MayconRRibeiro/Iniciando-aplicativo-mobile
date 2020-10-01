8515949142a73ea0f4107f707cc3d683387117c6
Mike <m1kros@hotmail.com>
2020-10-01 16:03:06 -0300

    Autenticação de usuários

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import { useAuth } from '../../hooks/auth';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -39,7 +41,10 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
+
   const navigation = useNavigation();
+
+  const { signIn } = useAuth();
 
   const handleSignIn = useCallback(async (data: SignInFormData) => {
     try {
@@ -56,10 +61,10 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-      // await SignIn({
-      //   email: data.email,
-      //   password: data.password,
-      // });
+      await signIn({
+        email: data.email,
+        password: data.password,
+      });
 
       // history.push('/dashboard');
     } catch (err) {
@@ -76,7 +81,7 @@ const SignIn: React.FC = () => {
         'Ocorreu um erro ao fazer login, cheque as credenciais.',
       );
     }
-  }, []);
+  }, [signIn]);
 
   return (
     <>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -81,7 +81,7 @@ const SignUp: React.FC = () => {
       );
     }
   },
-    [],
+    [navigation],
   );
 
   return (
