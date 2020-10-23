952fdc8b0e270c55e0152c521a35e3aa6d6bc4e2
Mike <m1kros@hotmail.com>
2020-10-23 16:56:21 -0300

    Mostrando horários em tela

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -102,7 +102,7 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Form ref={formRef} onSubmit={handleSubmit}>
+            <Form style={{ width: "100%" }} ref={formRef} onSubmit={handleSubmit}>
               <Input
                 autoCapitalize="words"
                 name="name"
