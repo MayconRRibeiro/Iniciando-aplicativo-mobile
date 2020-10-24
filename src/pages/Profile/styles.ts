692dd967252de75af601e23a150cbf7d91e4f152
Mike <m1kros@hotmail.com>
2020-10-24 18:50:19 -0300

    Criando tela de perfil

---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding-left: 30px;
+  padding-bottom: ${Platform.OS === 'android' ? 150 : 40}px;
+  padding-top: ${getStatusBarHeight() + 24 }px;
+  position: relative;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 24px 0;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 120px;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity`
+  margin-top: 32px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 186px;
+  height: 186px;
+  border-radius: 98px;
+  align-self: center;
+`;
